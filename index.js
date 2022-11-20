691985be24840f1a24a2f5a3e1904914efcc39d2
FukuInTheCode <fukulovecoding@gmail.com>
2022-11-20 15:32:45 +0100

    .

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+// The Collection class extends JavaScript's native Map class,
+// and includes more extensive, useful functionality. 
+// Collection is used to store and efficiently retrieve commands
+// for execution.
+client.commands = new Collection();
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
